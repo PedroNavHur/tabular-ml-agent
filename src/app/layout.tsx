2a257a2a5184e127b428d6b83ffb1a9befeade52
Pedro Navarrete <navhpedro@gmail.com>
2025-09-04 00:03:46 -0400

    Init NextJS Project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="sunset" className="overflow-x-hidden">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
